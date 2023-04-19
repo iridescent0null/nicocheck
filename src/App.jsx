263d4018b2c90c86ce82092e6cadc47f8ddd7f80
iridescent0null <mitome@Force-of-Negation.local>
2023-04-19 21:17:08 +0900

    first form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 import logo from "./logo.svg";
+import React, {setState,useState} from 'react';
 
-function App() {
+function App(props) {
+  const [username,setUsername] = useState('');
+  const handleSubmit = e =>{
+    e.preventDefault();
+    const data = {
+      username: username
+    };
+    alert(data.username);
+  };
   return (
     <div className="App" role="main">
       <article className="App-article">
@@ -15,13 +24,16 @@ function App() {
         >
           Learn React
         </a>
-
+        <form>
+            <input type="text" onChange={e => setUsername(e.target.value)}/>
+            <input type="submit" value="check!" onClick={handleSubmit}/>
+        </form>
         <div>
           {new Date().toLocaleString()}
         </div>
       </article>
     </div>
   );
-}
+};
 
 export default App;
